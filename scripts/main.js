7fd6c3fa5db6c272edeefab7b64a3a7864be9aea
Dong-Wo-Long <147408038+Dong-Wo-Long@users.noreply.github.com>
2024-08-11 03:23:57 +0900

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+importClass(java.lang.Class);
+
+const mod = Vars.mods.locateMod("bsm");
+
+mod.meta.displayName = "[#AD33FF]B[#800FFF]e[#AD33FF]tt[#530FFF]er[#800FFF]us[#530FFF]t[#380BA5]ry [#AD33FF]Mo[#800FFF]d";
+mod.meta.description = "[accent]RU:[] [gray]Очень опасный мод, который влияет на баланс и механику игры, добавляет новый контент в виде статус эффектов и оборонительных сооружений (башен, мин и т.д.).\n\nРекомендуется перед установкой сделать сохранение (чтобы откатиться назад), если в случае вам не понравится то, что может сделать данный мод. И лучше в самом деле не рискуйте лишний раз и поверьте на слово: [red]МОД. МЕНЯЕТ. МНОГОЕ.[]\n\n[accent]Важно:[] Описание некоторых юнитов не корректно, либо и вовсе неправильное.\n\n\n\n[accent]ENG:[] A very dangerous mod that affects the balance and mechanics of the game, adds new content in the form of status effects and defensive structures (towers, mines, etc.).\n\n\nPreferably before installing make a save (to roll back), if in case you do not like what this mod can do. And it is better to really do not take unnecessary risks and take my word for it: [red]MOD. CHANGES. MULTIPLE.[]\n\n[accent]Important:[] The description of some units is not correct or even incorrect.\n\n[accent]BDSM mod test subjects:[]\n[#8100cd]- inflarespirit (eve_light)\n[#fc8e6c]- ARmOrDer\n\n[accent]Special thanks:[]\n[#8F00B8]- 6matie9 (.miaofficial.)";
+mod.meta.author = "[accent]D[#9E5F00]&[accent]X";
+
+require("foreshadow");
+require("evilLaugh");
+require("oct");
+require("reconstructors/additiveReconstructor");
+require("reconstructors/multiplicativeReconstructor");
+require("reconstructors/exponentialReconstructor");
+require("reconstructors/tetrativeReconstructor");
